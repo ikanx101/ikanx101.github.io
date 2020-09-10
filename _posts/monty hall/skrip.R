ba7d8833d2a63f44366e58447a87a8f2fac6714c
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2020-09-10 19:09:18 +0700

    Update skrip.R

--- _posts/monty hall/skrip.R
+++ _posts/monty hall/skrip.R
@@ -3,13 +3,21 @@ setwd("~/Documents/ikanx101.com/_posts/monty hall")
 rm(list=ls())
 
 library(dplyr)
+library(tidyr)
 
 pintu = function(x){
-  hasil = sample(c(0,0,1),3,replace = F)
-  hasil = stringr::str_c(hasil,collapse = ",")
-  return(hasil)
+hasil = sample(c(0,0,1),3,replace=F)
+stringr::str_c(hasil,collapse=",")
 }
 
-data = data.frame(id = c(1:100))
-data$hasil = sapply(data$id,pintu)
+data = data.frame(id=c(1:900))
+
+data$sim = sapply(data$id,pintu)
+
+data =
+data %>%
+separate(sim,into=c("A","B","C"),sep=",") %>%
+mutate_if(is.character,as.numeric)
+
+summary(data)
                     
