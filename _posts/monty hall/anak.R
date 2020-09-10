7ac1fe28f203bce4507133c0e225ceaa10b2370c
ikanx101 <ikanx101@gmail.com>
2020-09-10 21:49:34 +0700

    nnew

---
+++ _posts/monty hall/anak.R
@@ -0,0 +1,9 @@
+rm(list=ls())
+
+total_boy = 0
+i = 0
+while(total_boy < 1){
+  anak = sample(c(0,1),1)
+  total_boy = total_boy+anak
+  i = i+1
+}
--- _posts/monty hall/skrip.R
+++ _posts/monty hall/skrip.R
@@ -1,23 +1,41 @@
-setwd("~/Documents/ikanx101.com/_posts/monty hall")
-
 rm(list=ls())
 
 library(dplyr)
 library(tidyr)
 
-pintu = function(x){
-hasil = sample(c(0,0,1),3,replace=F)
-stringr::str_c(hasil,collapse=",")
+babak = function(id){
+  # data awal
+  data = data.frame(pintu = c('a','b','c'),
+                    hadiah = sample(c("mobil",'zonk','zonk'))
+  )
+  # di mana pintu hadiah dan pintu apa yang dpilihi
+  data = 
+    data %>% 
+    mutate(pintu_isi_hadiah = ifelse(hadiah == "mobil",1,0),
+           pintu_pilihan = sample(c(0,0,1)))
+  # mencari pintu zonk untuk  dibuka
+  dummy = 
+    data %>% 
+    mutate(dummy = pintu_isi_hadiah + pintu_pilihan) %>% 
+    filter(dummy == 0) %>% 
+    select(pintu)
+  pintu_dibuka = sample(c(dummy$pintu),1)
+  data$pintu_dibuka = ifelse(data$pintu == pintu_dibuka,1,0)
+  # apakah pilihan kita berhasil?
+  data = 
+    data %>% filter(pintu_pilihan == 1)
+  mean(data$pintu_pilihan == data$pintu_isi_hadiah)
 }
 
-data = data.frame(id=c(1:900))
+simulasi = data.frame(id = c(1:900))
+simulasi$hasil = sapply(simulasi$id,babak)
 
-data$sim = sapply(data$id,pintu)
-
-data =
-data %>%
-separate(sim,into=c("A","B","C"),sep=",") %>%
-mutate_if(is.character,as.numeric)
-
-summary(data)
-                    
+library(ggplot2)
+simulasi %>% 
+  mutate(stay = cumsum(hasil)/id*100,
+         switch = 100-stay,
+         hasil = NULL) %>% 
+  reshape2::melt(id.vars = "id") %>% 
+  ggplot(aes(x = id,y = value)) +
+  geom_col(aes(fill = variable)) +
+  geom_hline(yintercept = 2/3*100)
