d593db93a5a03cfdd860c967247b73ef72eceb37
ikanx101 <ikanx101@gmail.com>
2020-10-20 14:07:34 +0700

    assets

--- _posts/tiktoks/skrip.R
+++ _posts/tiktoks/skrip.R
@@ -6,11 +6,26 @@ rm(list=ls())
 
 tk_init()
 
-hash_post <- tk_posts(scope = "hashtag", query = "SerunyaNutrisari", n = 300)
 
-trends <- tk_posts(scope = "trends", n = 200)
+hestek_setinggi_bintang = tk_posts(scope = "hashtag", query = "SetinggiBintang", n = 300)
+
+user_posts = tk_posts(scope = "user", query = "hiloteen", n = 50)
 
 # py_install("requests", pip = TRUE) kalau ada error boleh pake ini
 
 
-save(hash_post,hash_post_2,file = "toktok.rda")
+
+
+trends <- tk_posts(scope = "trends", n = 200)
+
+library(jsonlite)
+library(rjson)
+
+data = read_json("new.json")
+
+length(data$comments)
+
+data$comments[[1]]$text
+
+save(data,hestek_setinggi_bintang,user_posts,file = "toktok.rda")
+
