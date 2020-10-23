ff53b401cc1e9b2df1f1d23166960e2390bedff3
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2020-10-23 19:03:34 +0700

    Update Generate data.R

--- _posts/expss/bagian 1/Generate data.R
+++ _posts/expss/bagian 1/Generate data.R
@@ -6,7 +6,9 @@ n = 300
 gender = sample(c("pria","wanita"),n,replace = T, prob = c(.3,.7))
 usia = sample(c(25:40),n,replace = T)
 pernah = sample(c("ya","tidak"),n,replace = T, prob = c(.7,.3))
-tempat = sample(c("pasar","warung","minimarket","supermarket","online"),n,replace = T)
+rating = sample(c("sangat tidak suka","tidak suka","suka","sangat suka"),n,replace = T)
 
+tempat = sample(c("pasar","warung","minimarket","supermarket","online"),n*4,replace = T)
 
+data = data.frame()
 
