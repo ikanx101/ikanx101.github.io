dd8ef03f49e2005e2a73368ca2b0bc6cb3bc31a9
ikanx101 <ikanx101@gmail.com>
2020-10-23 22:44:55 +0700

    asset data

--- _posts/expss/bagian 1/Generate data.R
+++ _posts/expss/bagian 1/Generate data.R
@@ -2,13 +2,46 @@ rm(list=ls())
 
 library(dplyr)
 
-n = 300
+n = 1000
+kota = sample(c("Jakarta","Bandung","Tasikmalaya","Garut","Sukabumi"),n,replace = T)
 gender = sample(c("pria","wanita"),n,replace = T, prob = c(.3,.7))
 usia = sample(c(25:40),n,replace = T)
 pernah = sample(c("ya","tidak"),n,replace = T, prob = c(.7,.3))
 rating = sample(c("sangat tidak suka","tidak suka","suka","sangat suka"),n,replace = T)
 
-tempat = sample(c("pasar","warung","minimarket","supermarket","online"),n*4,replace = T)
+tempat = sample(c("pasar","warung","minimarket","supermarket","online"),n*2,replace = T)
 
-data = data.frame()
+data = data.frame(
+  id = c(1:n),
+  kota,
+  gender,
+  usia,
+  pernah,
+  rating
+)
 
+pilter = 
+  data %>%
+  filter(pernah == "ya") %>% 
+  select(id)
+
+new = data.frame(
+  id = sample(pilter$id,2000,replace = T),
+  tempat
+) %>% 
+  distinct() %>% 
+  arrange(id)
+
+new = 
+  new %>% 
+  group_by(id) %>% 
+  summarise(gabung = stringr::str_c(tempat,collapse = ",")) %>% 
+  ungroup()
+
+data = merge(data,new,all = T)
+
+data %>% 
+  tidyr::separate(gabung,
+                  into = c("tempat_1","tempat_2","tempat_3","tempat_4","tempat_5"),
+                  sep = ",") %>% 
+  write.csv("data survey.csv")
