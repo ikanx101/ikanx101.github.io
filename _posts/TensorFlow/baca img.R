1386a025333499611a4873aa399fe965ac7d3f3c
ikanx101 <ikanx101@gmail.com>
2020-11-07 12:36:50 +0700

    updates

--- _posts/TensorFlow/baca img.R
+++ _posts/TensorFlow/baca img.R
@@ -1,7 +1,14 @@
 setwd("~/Documents/ikanx101/_posts/TensorFlow/Poto Set")
+setwd("~/Documents/ikanx101/_posts/TensorFlow/")
 
+load("bahan blog.rda")
 img = list.files()
 
-train = sapply(img,readImage)
-par(mfrow=c(4,8))
-for(i in 1:31) plot(train[[i]])
+train = sapply(img[-id_train],readImage)
+
+par(mfrow=c(2,3))
+for(i in 1:6) plot(train[[i]])
+
+
+
+train = train[[id_train]]
