4c64c8bdfb9aea84f16a528367179a193e8605d9
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2020-11-08 09:23:21 +0700

    update

--- _posts/Monte Carlo/Hitung Pi/Skrip simulasi.R
+++ _posts/Monte Carlo/Hitung Pi/Skrip simulasi.R
@@ -34,14 +34,14 @@ hitung_pi = function(n){
 simulasi = data.frame(
   n = c(10,20,50,75,100,400,600,999,1100,2000,
         3000,5000,7500,10000,20000,30000,500000,
-        1000000),
+        1000000,2000000,4000000),
   pi = 0
 )
 
 simulasi$pi = sapply(simulasi$n,hitung_pi)
 simulasi$beda = pi - simulasi$pi
-
 simulasi
+
 
 simulasi %>%
     ggplot(aes(x = n,
@@ -58,3 +58,51 @@ simulasi %>%
                  label = paste0("Pi ~ ",simulasi$pi[18],
                                 "\nselisih dengan Pi aslinya = ",round(simulasi$beda[18],7))
                  )
+
+# ======================
+# Ini bagian awal
+# rm(list=ls())
+
+# beda pi dengan 22/7
+pi - (22/7)
+
+# beda pi dengan 3.14
+pi - 3.14
+
+# pendekatan saya jauh lebih akurat dibandingkan 22/7 dan 3.14
+
+# bikin fungsi utk generate data
+  # lalu kita rbind
+generate_sim = function(n){
+  data = data.frame(
+  x = runif(n),
+  y = runif(n),
+  iterasi = n
+  ) %>%
+  mutate(penanda = ifelse(x^2 + y^2 <= 1, 1, 0))
+  return(data)
+}
+
+# sekarang kita bikin datasetnya
+n_sim = c(10,20,50,100,400,1000,5000)
+
+# kita mulai iterasinya
+i = 1
+data_sim = generate_sim(n_sim[i])
+
+for(i in 2:length(n_sim)){
+  temp = generate_sim(n_sim[i])
+  data_sim = rbind(data_sim,temp)
+}
+
+# yuk kita bikin dulu titiknya
+data_sim %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_point(aes(color = penanda)) +
+    facet_wrap(~iterasi) +
+    theme_minimal() +
+    labs(x = "x",
+         y = "y",
+         title = "Proses Menghitung L1\nDengan cara generating random dots",
+         subtitle = "Berapa banyak random dots yang dibuat:") +
+         theme(legend.position = "none")
