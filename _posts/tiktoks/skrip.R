10220f2a456b7e667b0070e436a96b0e1591179b
ikanx101 <ikanx101@gmail.com>
2020-11-28 15:20:04 +0700

    updates

--- _posts/tiktoks/skrip.R
+++ _posts/tiktoks/skrip.R
@@ -5,16 +5,10 @@ library(stringr)
 rm(list=ls())
 
 tk_init()
-
-
 hestek_setinggi_bintang = tk_posts(scope = "hashtag", query = "SetinggiBintang", n = 300)
-
 user_posts = tk_posts(scope = "user", query = "hiloteen", n = 50)
 
 # py_install("requests", pip = TRUE) kalau ada error boleh pake ini
-
-
-
 
 trends <- tk_posts(scope = "trends", n = 200)
 
@@ -24,8 +18,5 @@ library(rjson)
 data = read_json("new.json")
 
 length(data$comments)
-
 data$comments[[1]]$text
-
 save(data,hestek_setinggi_bintang,user_posts,file = "toktok.rda")
-
