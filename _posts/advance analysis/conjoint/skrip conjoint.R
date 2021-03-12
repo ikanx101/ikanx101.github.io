e050816314abf5da31703d8da508c199a7ddd79b
ikanx101 <ikanx101@gmail.com>
2021-03-13 05:11:40 +0700

    updates

--- _posts/advance analysis/conjoint/skrip conjoint.R
+++ _posts/advance analysis/conjoint/skrip conjoint.R
@@ -16,10 +16,31 @@ factdesign = caFactorialDesign(data=experiment,type="orthogonal")
 factdesign 
 
 prof = caEncodedDesign(design=factdesign)
-prof
+colnames(prof) = c("kamera","lock","charging")
+
+# buat ngeliat orthogonal atau gak
+print(round(cov(prof),5))
+print(round(cor(prof),5))
 
 case_level = c(kamera,lock,charging)
 case_level
 
-save(experiment,factdesign,prof,case_level,
+# buat responden palsu
+jawab = function(){
+  sample(10,9,replace = T)
+}
+
+for(i in 1:10){
+  nam = paste0("resp",i)
+  tes = jawab()
+  assign(nam,tes)
+}
+
+responses = data.frame(resp1,resp2,resp3,resp4,resp5,resp6,resp7,resp8,resp9,resp10)
+responses_print = data.frame(factdesign,responses)
+
+save(experiment,factdesign,prof,case_level,responses,responses_print,
      file = "bahan blog.rda")
+
+# bikin kmeans
+aSegmentation(y=responses, x=prof, c=3)
