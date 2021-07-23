389ed194e778646eb8ce0ff4850b4297222cd8c9
ikanx101 <ikanx101@gmail.com>
2021-07-23 13:44:26 +0700

    update assets

--- _posts/gtrends/bear brand/script.R
+++ _posts/gtrends/bear brand/script.R
@@ -1,6 +1,6 @@
 library(gtrendsR)
 
-data = gtrends(c("susu beruang","bear brand"),geo = "ID")
+data = gtrends(c("bakmi","mie ayam"),geo = "ID")
 
 str(data)
 
@@ -9,3 +9,15 @@ data_city = data$interest_by_city
 data_query = data$related_queries
 
 save(data,data_int,data_city,data_query,file = "bahan blog.rda")
+
+library(ggplot2)
+data_city %>% 
+  filter(!is.na(hits)) %>% 
+  ggplot(aes(y = reorder(location,hits),
+             x = hits,
+             fill = keyword)) +
+  geom_col() +
+  theme_minimal() +
+  labs(title = "Trend Pencarian Bakmi dan Mie Ayam per Kota",
+       subtitle = "source: Google Trends 9 Juli 2021 14:30 WIB") +
+  theme(axis.title = element_blank())
