4db14e7f68d928c913ca360826be1ca5cb4e9a6b
ikanx101 <ikanx101@gmail.com>
2022-02-02 10:32:12 +0700

    todo

--- _posts/Monte Carlo/binomo/skrip.R
+++ _posts/Monte Carlo/binomo/skrip.R
@@ -3,9 +3,9 @@ rm(list=ls())
 library(dplyr)
 
 # binomo
-# kalau benar dapat 80%
-# kalau salah langsung hangus
-# berikut ini function nya
+  # kalau benar dapat 80%
+  # kalau salah langsung hangus
+  # berikut ini function nya
 binomo = function(init_money){
   tebak = sample(c(0,1),2,replace = T, prob = c(.5,.5))
   pengali = ifelse(tebak[1] == tebak[2], 1.8, 0)
@@ -15,30 +15,47 @@ binomo = function(init_money){
 
 # kita buat function iterasinya
 simu_liu = function(){
+  # initial money
   money = 100000
   i = 1
-
+  # siapin rumah
   idx = c(i)
   uang = c(money)
-  
+  # iterasi tebakan binomo
   while(money > 0){
     money = binomo(money)
     i = i + 1
     idx = c(idx,i)
     uang = c(uang,money)
   }
-
+  # hasil akhir
   df = data.frame(idx,uang)
   return(df)
 }
 
-iter_max = 5000
+# iterasi maksimum
+iter_max = 4000
 
+# bikin rumah untuk hasil simulasi
 rumah = vector("list",iter_max)
 
+# iterasi simulasi
 for(i in 1:iter_max){
   rumah[[i]] = simu_liu()
 }
 
-rumah
-save(rumah,file = "simulasi.rda")
+# data final gabung semua
+data_final = do.call(bind_rows,rumah)
+data_final$exp = NA
+data_final$exp[1] = 1
+idx = 1
+
+for(i in 2:nrow(data_final)){
+  data_final$exp[i] = ifelse(data_final$idx[i] == 1,
+                             data_final$exp[i-1] + 1,
+                             data_final$exp[i-1])
+}
+
+data_final
+
+save(data_final,file = "simulasi.rda")
