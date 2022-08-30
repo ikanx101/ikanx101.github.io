b6a706ed2ea37bdbc3727399208f387fb147dcff
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-08-31 06:21:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
+sudo apt install gnupg
+sudo apt install ca-certificates
+sudo apt install nano
+sudo apt install gdebi-core
+sudo apt-get install build-essential
 sudo apt install libcurl4-openssl-dev libssl-dev libxml2-dev
-
-# update indices
-sudo apt update -qq
-# install two helper packages we need
 sudo apt install --no-install-recommends software-properties-common dirmngr
-# add the signing key (by Michael Rutter) for these repos
-# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
-# Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install r-base-dev
+
