4ec85a0711ee4e1cfdd830eb147483e11c0648c3
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-08-31 06:42:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
 sudo apt update
 sudo apt upgrade
