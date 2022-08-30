0affdddf7ac3f69874b7737f94d93b5348d7ab09
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2022-08-31 06:34:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,7 @@
-sudo apt install gnupg
-sudo apt install ca-certificates
-sudo apt install nano
-sudo apt install gdebi-core
-sudo apt-get install build-essential
-sudo apt install libcurl4-openssl-dev libssl-dev libxml2-dev
-sudo apt install --no-install-recommends software-properties-common dirmngr
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-
 sudo apt update
 sudo apt upgrade
-
-sudo apt install r-base-dev
 
