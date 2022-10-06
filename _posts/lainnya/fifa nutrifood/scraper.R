6c10c76952303cf1c12133f9ed920a6badec13b7
ikanx101 <ikanx101@gmail.com>
2022-10-06 05:36:58 +0000

    update data hasil scrape

--- _posts/lainnya/fifa nutrifood/scraper.R
+++ _posts/lainnya/fifa nutrifood/scraper.R
@@ -6,11 +6,25 @@ library(dplyr)
 # informasi dan data terkait final piala dunia
 url = "https://en.wikipedia.org/wiki/List_of_FIFA_World_Cup_finals"
 final = url %>% read_html() %>% html_table(fill = T)
-
-# informasi dan data terkait piala dunia
-url = "https://en.wikipedia.org/wiki/2022_FIFA_World_Cup"
-df_pildun = url %>% read_html() %>% html_table(fill = T)
+# data turunan
+df_final = final[[4]]
+df_winners = final[[5]]
+df_winners_uefa_conmebol = final[[6]]
 
 # informasi dan data terkait kualifikasi piala dunia
 url = "https://en.wikipedia.org/wiki/2022_FIFA_World_Cup_qualification"
 df_qualification = url %>% read_html() %>% html_table(fill = T)
+# data turunan
+df_negara_terkualifikasi = df_qualification[[2]]
+df_asia_kualifikasi_1 = df_qualification[[5]]
+df_asia_kualifikasi_2 = df_qualification[[6]]
+df_amerika_kualifikasi = df_qualification[[9]]
+df_conmebol_kualifikasi = df_qualification[[10]]
+
+
+# kita save dulu
+save(df_final,df_winners,df_winners_uefa_conmebol,
+     df_negara_terkualifikasi,
+     df_asia_kualifikasi_1,df_asia_kualifikasi_2,
+     df_amerika_kualifikasi,df_conmebol_kualifikasi,
+     file = "related_data.rda")
