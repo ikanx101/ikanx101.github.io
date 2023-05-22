1983d2f210ed80ad1658617b400cb84794516e1b
ikanx101 <ikanx101@gmail.com>
2023-05-22 11:32:33 +0000

    Soal terbaru

---
+++ _posts/linear problem/post 16/Soal.R
@@ -0,0 +1,23 @@
+rm(list=ls())
+
+library(dplyr)
+library(tidyr)
+
+# diketahui
+# banyak team member 5
+# banyak hari 5
+# banyak jam kerja perhari 7
+# maks boleh lembur 2 jam, maks 2 x seminggu
+
+4*4*7
+
+n_kerjaan = 30
+df_req    = data.frame(kegiatan    = paste("kegiatan",
+                                           1:n_kerjaan),
+                       waktu_kerja = sample(6,
+                                            n_kerjaan,
+                                            replace = T))
+
+sum(df_req$waktu_kerja)
+
+save(df_req,file = "soal.rda")
