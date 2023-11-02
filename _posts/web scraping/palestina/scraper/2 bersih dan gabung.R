b33b0a8030f52d5488abc15b66e740ab55f9a653
ikanx101 <ikanx101@gmail.com>
2023-11-02 14:44:01 +0700

    bersih dan gabung

--- _posts/web scraping/palestina/scraper/2 bersih dan gabung.R
+++ _posts/web scraping/palestina/scraper/2 bersih dan gabung.R
@@ -43,3 +43,92 @@ bersihkan = function(input){
 df_detik  = bersihkan(df_detik)
 df_kompas = bersihkan(df_kompas)
 df_cnn    = bersihkan(df_cnn)
+
+
+
+
+dia_wc = 
+  pre_data %>% 
+  filter(produk == "Diabetasol") %>% 
+  unnest_tokens("words",komen) %>% 
+  group_by(words) %>% 
+  count(sort = T) %>% 
+  ungroup() %>% 
+  rename(word = words,
+         freq = n)
+
+png("dia cloud.png",width = 900,height = 750,res = 150)
+wordcloud::wordcloud(words = dia_wc$word, 
+                     freq = dia_wc$freq, 
+                     min.freq = 0,
+                     max.words=150, 
+                     use.r.layout=FALSE,
+                     random.order=FALSE,
+                     rot.per = .25,
+                     colors=brewer.pal(5, "Dark2"))
+dev.off()
+
+
+
+
+
+```{r,fig.retina = 10,echo=FALSE,message=FALSE,warning=FALSE}
+pre_data %>% 
+  filter(produk == "Diabetasol") %>% 
+  unnest_tokens("bigrams",komen,token = "ngrams",n = 2) %>% 
+  group_by(bigrams) %>% 
+  count(sort = T) %>% 
+  ungroup() %>% 
+  filter(n > 3) %>% 
+  separate(bigrams,into = c("from","to"),sep = " ") %>% 
+  graph_from_data_frame() %>% 
+  ggraph(layout = 'fr') +
+  geom_edge_bend(aes(edge_alpha=n),
+                 show.legend = F,
+                 color='darkred') +
+  geom_node_point(size=1,color='steelblue') +
+  geom_node_text(aes(label=name),alpha=0.4,size=3,repel = T) +
+  theme_void()
+```
+
+
+```{r,echo=FALSE,warning=FALSE,message=FALSE}
+library(NLP)
+library(tm)
+library(topicmodels)
+
+topik_dia = pre_data %>% filter(produk == "Diabetasol")
+NAME = topik_dia$komen
+NAME = Corpus(VectorSource(NAME))
+NAME = tm_map(NAME, content_transformer(tolower))
+NAME = tm_map(NAME,removePunctuation)
+NAME = tm_map(NAME, stripWhitespace)
+NAME = tm_map(NAME,removeNumbers)
+tdm = TermDocumentMatrix(NAME)
+dtm = as.DocumentTermMatrix(tdm) 
+rowTotals = apply(dtm , 1, sum) 
+dtm = dtm[rowTotals> 0, ]           
+lda = LDA(dtm, k = 5)  
+term = terms(lda, 3)  
+term = apply(term, MARGIN = 2, paste, collapse = ", ")
+print("5 Topics dari Reviews Diabetasol")
+cbind(term)
+
+
+topik_dia = pre_data %>% filter(produk == "TS Stevia")
+NAME = topik_dia$komen
+NAME = Corpus(VectorSource(NAME))
+NAME = tm_map(NAME, content_transformer(tolower))
+NAME = tm_map(NAME,removePunctuation)
+NAME = tm_map(NAME, stripWhitespace)
+NAME = tm_map(NAME,removeNumbers)
+tdm = TermDocumentMatrix(NAME)
+dtm = as.DocumentTermMatrix(tdm) 
+rowTotals = apply(dtm , 1, sum) 
+dtm = dtm[rowTotals> 0, ]           
+lda = LDA(dtm, k = 5)  
+term = terms(lda, 3)  
+term = apply(term, MARGIN = 2, paste, collapse = ", ")
+print("5 Topics dari Reviews Tropicana Slim Stevia")
+cbind(term)
+```
